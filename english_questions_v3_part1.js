4673f4533bbd552c194d594e602eead1f426c924
ENGR007 <thranli07@gmail.com>
2025-11-09 01:05:01 +0800

    Update english_questions_v3_part1.js

--- english_questions_v3_part1.js
+++ english_questions_v3_part1.js
@@ -1,1 +1,74 @@
-code
+/*
+---------------------------------------------------------
+ English Explorer – v3 Dataset
+ Part 1: Grammar & Vocabulary  (Block 1 of 2)
+ Structure:
+   grammar_easy, grammar_average
+---------------------------------------------------------
+*/
+const questionBankPart1 = {
+  grammar_easy: [
+    { q: "She _____ to school every day.", opts:["go","goes","going"], ans:"goes" },
+    { q: "They _____ playing football now.", opts:["is","are","was"], ans:"are" },
+    { q: "He didn’t _____ his homework.", opts:["do","does","did"], ans:"do" },
+    { q: "We _____ dinner at seven o’clock yesterday.", opts:["have","had","has"], ans:"had" },
+    { q: "The cat _____ under the chair.", opts:["is","are","was"], ans:"is" },
+    { q: "I _____ a pencil in my bag.", opts:["has","have","had"], ans:"have" },
+    { q: "The boy _____ his shoes before running.", opts:["tie","ties","tied"], ans:"tied" },
+    { q: "There _____ many stars in the sky.", opts:["is","are","was"], ans:"are" },
+    { q: "She _____ already finished her project.", opts:["have","has","had"], ans:"has" },
+    { q: "My parents _____ watching television.", opts:["is","are","was"], ans:"are" },
+    { q: "The children _____ excited about the trip.", opts:["is","are","was"], ans:"are" },
+    { q: "We _____ going to the library today.", opts:["is","are","am"], ans:"are" },
+    { q: "The sun _____ in the east.", opts:["rise","rises","rising"], ans:"rises" },
+    { q: "I _____ never seen snow before.", opts:["has","have","had"], ans:"have" },
+    { q: "She _____ a beautiful dress yesterday.", opts:["wear","wears","wore"], ans:"wore" },
+    { q: "Tom _____ to play the piano after dinner.", opts:["like","likes","liked"], ans:"likes" },
+    { q: "The dogs _____ barking loudly.", opts:["is","are","was"], ans:"are" },
+    { q: "It _____ raining when I left home.", opts:["is","was","were"], ans:"was" },
+    { q: "We _____ the museum last Sunday.", opts:["visit","visited","visiting"], ans:"visited" },
+    { q: "The teacher _____ a story to us.", opts:["tell","tells","told"], ans:"told" },
+    { q: "The flowers _____ lovely.", opts:["smell","smells","smelled"], ans:"smell" },
+    { q: "Each student _____ a worksheet.", opts:["have","has","having"], ans:"has" },
+    { q: "She _____ her homework before dinner.", opts:["finish","finishes","finished"], ans:"finished" },
+    { q: "There _____ a lot of milk in the jug.", opts:["are","is","were"], ans:"is" },
+    { q: "He _____ up early every morning.", opts:["get","gets","got"], ans:"gets" },
+    { q: "We _____ to the beach last weekend.", opts:["go","went","going"], ans:"went" },
+    { q: "They _____ reading a new storybook.", opts:["is","are","was"], ans:"are" },
+    { q: "My brother _____ football very well.", opts:["play","plays","played"], ans:"plays" },
+    { q: "The children _____ happy to see the clown.", opts:["was","were","is"], ans:"were" },
+    { q: "She _____ cooking when her friend arrived.", opts:["was","were","is"], ans:"was" }
+  ],
+
+  grammar_average: [
+    { q: "If I _____ time, I would help you.", opts:["have","had","has"], ans:"had" },
+    { q: "She speaks as if she _____ everything.", opts:["know","knew","knows"], ans:"knew" },
+    { q: "Neither the teacher nor the students _____ present.", opts:["was","were","are"], ans:"were" },
+    { q: "Everybody _____ welcome to join the event.", opts:["are","is","were"], ans:"is" },
+    { q: "The ducks _____ swimming in the pond.", opts:["is","are","was"], ans:"are" },
+    { q: "Each of the boys _____ done his work.", opts:["have","has","had"], ans:"has" },
+    { q: "She prefers tea _____ coffee.", opts:["than","to","over"], ans:"to" },
+    { q: "They have been waiting _____ an hour.", opts:["since","for","from"], ans:"for" },
+    { q: "Please give the keys to _____.", opts:["he","him","his"], ans:"him" },
+    { q: "Neither of the answers _____ correct.", opts:["are","is","were"], ans:"is" },
+    { q: "The students were tired, _____ they continued working.", opts:["so","but","because"], ans:"but" },
+    { q: "I will go for a walk _____ it stops raining.", opts:["if","because","so"], ans:"if" },
+    { q: "She bought apples _____ oranges at the market.", opts:["and","or","but"], ans:"and" },
+    { q: "We stayed at home _____ it was raining.", opts:["because","so","but"], ans:"because" },
+    { q: "He runs _____ than his brother.", opts:["fast","faster","fastest"], ans:"faster" },
+    { q: "This is the _____ movie I have ever seen.", opts:["funny","funnier","funniest"], ans:"funniest" },
+    { q: "Either my mother or my sisters _____ cooking now.", opts:["is","are","was"], ans:"are" },
+    { q: "Both of the girls _____ good at dancing.", opts:["is","are","was"], ans:"are" },
+    { q: "The news _____ surprising to everyone.", opts:["is","are","were"], ans:"is" },
+    { q: "The book on the table _____ mine.", opts:["is","are","were"], ans:"is" },
+    { q: "He behaves as though he _____ the boss.", opts:["is","was","were"], ans:"were" },
+    { q: "If she _____ earlier, she would catch the bus.", opts:["leave","left","leaves"], ans:"left" },
+    { q: "The man _____ the gate is my uncle.", opts:["who","which","where"], ans:"who" },
+    { q: "The teacher asked us _____ we had finished the task.", opts:["what","if","that"], ans:"if" },
+    { q: "I am not sure _____ he will come.", opts:["if","that","who"], ans:"if" },
+    { q: "The child cried because she _____ her toy.", opts:["lost","lose","loses"], ans:"lost" },
+    { q: "You may go home after you _____ your work.", opts:["finish","finished","finishes"], ans:"finish" },
+    { q: "The bus usually _____ at this stop.", opts:["stop","stops","stopped"], ans:"stops" },
+    { q: "She sings _____ than her friend.", opts:["beautiful","more beautifully","most beautiful"], ans:"more beautifully" },
+    { q: "My uncle has lived here _____ five years.", opts:["for","since","from"], ans:"for" }
+  ],
